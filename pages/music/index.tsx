3e844dbbcfd749b5ee024c23779f2f6a1b16b99a
Marcelo Mesquita <contato@marcelomesquita.com>
2021-04-06 23:14:37 -0300

    padronizacao das páginas

--- pages/music/index.tsx
+++ pages/music/index.tsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
 import Link from "next/link";
+import React from "react";
 import Container from "../../components/layout/container"
 import Breadcrumb from "../../components/shared/breadcrumb";
 import Pagination from "../../components/shared/pagination";
 import { Music } from "../../models/music";
 import { PROJECT } from "../../models/project";
+
+interface MusicsProps {
+	music: Music
+}
+
+interface MusicsState {
+	musics: Array<Music>;
+}
 
 interface MusicsResponse {
 	message: string;
@@ -21,51 +30,63 @@ export async function getStaticProps(context) {
 	}
 }
 
-function MusicsPage(props) {
-	const pageTitle = "Músicas";
+class MusicsPage extends React.Component<MusicsProps, MusicsState> {
+	pageTitle: string = "Músicas";
 
-	return (
-		<Container>
-			<Head>
-				<title>{pageTitle} | {PROJECT.TITLE}</title>
-			</Head>
+	constructor(props) {
+		super(props);
 
-			<div className="container is-widescreen">
-				<section className="section">
-					<Breadcrumb />
+		this.state = {
+			musics: props.musics
+		}
+	}
 
-					<h1 className="title">{pageTitle}</h1>
-					<h2 className="subtitle">Não encontrou a música que queria? Que tal <Link href="/music/new"><a>cadastrá-la!</a></Link></h2>
+	render() {
+		let musics: Array<Music> = this.state.musics;
 
-					<div className="columns is-multiline">
-						{props.musics.map((music) => { return (
-							<div key={music.slug} className="column is-4">
-								<Link href={`/music/${music.slug}`}>
-									<a>
-										<div className="card">
-											<div className="card-image">
-												<figure className="image is-4by3">
-													<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-												</figure>
-											</div>
-											<div className="card-content">
-												<div className="content">
-													<p className="title is-5">{music.title}</p>
-													<p className="subtitle is-6">{music.artist}</p>
+		return (
+			<Container>
+				<Head>
+					<title>{this.pageTitle} | {PROJECT.TITLE}</title>
+				</Head>
+
+				<div className="container is-widescreen">
+					<section className="section">
+						<Breadcrumb />
+
+						<h1 className="title">{this.pageTitle}</h1>
+						<h2 className="subtitle">Não encontrou a música que queria? Que tal <Link href="/music/new"><a>cadastrá-la!</a></Link></h2>
+
+						<div className="columns is-multiline">
+							{musics.map((music) => { return (
+								<div key={music.slug} className="column is-4">
+									<Link href={`/music/${music.slug}`}>
+										<a>
+											<div className="card">
+												<div className="card-image">
+													<figure className="image is-4by3">
+														<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+													</figure>
+												</div>
+												<div className="card-content">
+													<div className="content">
+														<p className="title is-5">{music.title}</p>
+														<p className="subtitle is-6">{music.artist}</p>
+													</div>
 												</div>
 											</div>
-										</div>
-									</a>
-								</Link>
-							</div>
-						) })}
-					</div>
+										</a>
+									</Link>
+								</div>
+							) })}
+						</div>
 
-					<Pagination />
-				</section>
-			</div>
-		</Container>
-	)
+						<Pagination />
+					</section>
+				</div>
+			</Container>
+		)
+	}
 }
 
 export default MusicsPage;
