71cb078da1d6703ffc2183164ca5f8d4769531ce
Marcelo Mesquita <contato@marcelomesquita.com>
2021-04-16 11:19:53 -0300

    correção da url do projeto

--- project.tsx
+++ project.tsx
@@ -3,7 +3,7 @@ import packageJson from './package.json';
 export const PROJECT = {
 	TITLE: "DrumTab",
 	DESCRIPTION: "The easiest way to read and write drum tabs!",
-	URL: "http://localhost:3000",
+	URL: "http://drumtab.club",
 	AUTHOR: packageJson.author,
 	VERSION: packageJson.version
 }
